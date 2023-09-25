f8aa9eaa5099db6afc1d418eb55bd5684b3720ec
PolYarik <polyarik@pm.me>
2023-09-26 02:39:49 +0700

    ✨ add test query to LeetCode API

--- src/main.py
+++ src/main.py
@@ -2,5 +2,18 @@ import requests
 
 
 def getLeetCodeData(username: str):
-    # ...
-    return "Hello!"
+    base_url = "https://leetcode.com/graphql"
+
+    query_data = f'matchedUser(username: "{username}") {{username}}'
+    # query_data = 'matchedUser(username: "PolYarik") {username}'
+    print(query_data)
+
+    full_url = f"{base_url}?query=query{{{query_data}}}"
+    response = requests.get(full_url)
+
+    print(response.status_code)
+    print(response)
+
+    json = response.json()
+    data = {"username": json["data"]["matchedUser"]["username"]}
+    return data
