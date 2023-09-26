c23373fb5c60b0a2d4aba4547fa9af01664fff40
PolYarik <polyarik@pm.me>
2023-09-26 22:31:24 +0700

    ✨ construct query body from config dictionary

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 leetcode_name = "PolYarik"  # your LeetCode username
 
-# test
-query_fields = " username\
-    profile {\
-        ranking\
-        reputation\
-    }"  # data requested from LeetCode
+query_fields = {  # data requested from LeetCode
+    "general": [
+        "username",
+        # ...
+    ],
+    "profile": [
+        "ranking",
+        "reputation",
+        # ...
+    ]
+    # ...
+}
 
 
 # TODO: TOKEN - the Telegram bot token
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import requests
 
 
-def getLeetCodeData(username: str, query_fields):  # set[str]):
-    base_url = "https://leetcode.com/graphql"
+BASE_URL = "https://leetcode.com/graphql"
 
-    query_data = f'matchedUser(username: "{username}") {{{query_fields}}}'
-    print(query_data)
 
-    full_url = f"{base_url}?query=query{{{query_data}}}"
+def getLeetCodeData(username: str, query_fields: dict):
+    query_body = constructQueryBody(query_fields)
+    query_data = f'matchedUser(username: "{username}") {{{query_body}}}'
+    full_url = f"{BASE_URL}?query=query{{{query_data}}}"
     print(full_url)
 
     response = requests.get(full_url)
@@ -15,6 +15,18 @@ def getLeetCodeData(username: str, query_fields):  # set[str]):
     print(response)
 
     json = response.json()
-    # data = {"username": json["data"]["matchedUser"]["username"]}
-    data = json  # temp
+    # TODO: build result
+    data = json["data"]["matchedUser"]
     return data
+
+
+def constructQueryBody(query_fields: dict) -> str:
+    query_body = ""
+
+    for field, values in query_fields.items():
+        if field == "general":
+            query_body += " ".join(values) + " "
+        else:
+            query_body += f'{field} {{{" ".join(values)}}}' + " "
+
+    return query_body
